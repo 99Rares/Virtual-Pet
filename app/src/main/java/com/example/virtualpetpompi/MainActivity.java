9744de2dafdcf6c439b9e5e9b2b52072df3f62cc
AndreiSiu <vasiu_andrei2000@yahoo.com>
2021-11-15 14:44:57 +0200

    Permission

--- app/src/main/java/com/example/virtualpetpompi/MainActivity.java
+++ app/src/main/java/com/example/virtualpetpompi/MainActivity.java
@@ -1,34 +1,51 @@
 package com.example.virtualpetpompi;
 
+import android.Manifest;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.icu.util.Calendar;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
-import android.app.AlarmManager;
-import android.app.PendingIntent;
-import android.content.Intent;
-import android.content.SharedPreferences;
-import android.icu.util.Calendar;
-import android.os.Build;
-import android.os.Bundle;
-import android.text.format.Time;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.content.Context;
-import android.hardware.Sensor;
-import android.hardware.SensorEventListener;
-import android.hardware.SensorManager;
-import android.widget.TextView;
-import android.widget.Toast;
-import android.hardware.SensorEvent;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
-    private Button menuBtn;
+    /**
+     * Opens the shop
+     */
+    private Button shopBtn;
+
+    /**
+     * opens the settings
+     */
+    private Button settingsBtn;
+    private FloatingActionButton menuBtn;
     private CardView menuPanel;
+
     private TextView nrSteps;
-    private CardView insideMenuPanel;
+
     private SensorManager sensorManager = null;
     private boolean running = false;
     private float totalSteps = 0;
@@ -46,12 +63,19 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         resetSteps();
         onWakeUp();
         saveData();
-        menuBtn.setOnClickListener(v -> menuPanel.setVisibility(View.VISIBLE));
+        openMenuPanel();
 
-        menuPanel.setOnClickListener(v -> menuPanel.setVisibility(View.GONE));
+        openSettings();
+        openShop();
+        requestActivityRecognition();
 
+        menuPanel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                menuPanel.setVisibility(View.GONE);
+            }
+        });
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-
 
 
     }
@@ -95,7 +119,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         SharedPreferences sharedPreferences = getSharedPreferences("myPrefs", Context.MODE_PRIVATE);
 
         SharedPreferences.Editor editor = sharedPreferences.edit();
-        editor.putFloat("total",totalSteps);
+        editor.putFloat("total", totalSteps);
         editor.putFloat("prev", previousTotalSteps);
         editor.apply();
     }
@@ -103,9 +127,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     private void loadData() {
 
         SharedPreferences sharedPreferences = getSharedPreferences("myPrefs", Context.MODE_PRIVATE);
-        float savedNumber = sharedPreferences.getFloat("key1", 0);
-
-        Log.d("MainActivity", "$savedNumber");
+        float savedNumber = sharedPreferences.getFloat("prev", 0);
 
         previousTotalSteps = savedNumber;
     }
@@ -122,25 +144,77 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     }
 
     private void initData() {
-        menuBtn = (Button) findViewById(R.id.menuBtn);
-        menuPanel = (CardView) findViewById(R.id.menuPanel);
-        insideMenuPanel = (CardView) findViewById(R.id.insideMenuPanel);
+        menuBtn = findViewById(R.id.menuBtn);
+        shopBtn = (Button) findViewById(R.id.shopBtn);
+        settingsBtn = (Button) findViewById(R.id.settingsBtn);
+
+        menuPanel = (CardView) findViewById(R.id.insideMenuPanel);
     }
+
     @RequiresApi(api = Build.VERSION_CODES.N)
 
-    private void onWakeUp(){
-        Context context=getApplicationContext();
+    private void onWakeUp() {
+        Context context = getApplicationContext();
         Calendar calendar = Calendar.getInstance();
         calendar.set(Calendar.HOUR_OF_DAY, 14);
         calendar.set(Calendar.MINUTE, 2);
         calendar.set(Calendar.SECOND, 0);
         calendar.set(Calendar.MILLISECOND, 0);
         PendingIntent pi = PendingIntent.getService(context, 0,
-                new Intent(context, ResetSteps.class),PendingIntent.FLAG_UPDATE_CURRENT);
+                new Intent(context, ResetSteps.class), PendingIntent.FLAG_UPDATE_CURRENT);
         AlarmManager am = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
         am.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(),
                 AlarmManager.INTERVAL_DAY, pi);
     }
 
+    private void openMenuPanel() {
+        menuBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                menuPanel.setVisibility(View.VISIBLE);
+            }
+        });
+    }
+
+    private void openShop() {
+        shopBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //startActivity(new Intent(MainActivity.this, ShopActivity.class));
+            }
+        });
+    }
+
+    private void openSettings() {
+        settingsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //startActivity(new Intent(MainActivity.this, SettingsActivity.class));
+            }
+        });
+    }
+
+    private void requestActivityRecognition() {
+        Dexter.withContext(this)
+                .withPermission(Manifest.permission.ACTIVITY_RECOGNITION)
+                .withListener(new PermissionListener() {
+                    @Override
+                    public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
+                        loadData();
+                        saveData();
+                    }
+
+                    @Override
+                    public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
+                        Toast.makeText(MainActivity.this, "Plz allow sensor to run", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
+                        permissionToken.continuePermissionRequest();
+                    }
+                })
+                .check();
+    }
 
 }
