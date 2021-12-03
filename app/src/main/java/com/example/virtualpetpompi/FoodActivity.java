7d251686bbcb9862659d28c929deb44960b6ee41
AndreiSiu <vasiu_andrei2000@yahoo.com>
2021-12-03 14:00:43 +0200

    Food designs!!
    Pompi can now eat all kinds of food :D

--- app/src/main/java/com/example/virtualpetpompi/FoodActivity.java
+++ app/src/main/java/com/example/virtualpetpompi/FoodActivity.java
@@ -28,7 +28,9 @@ public class FoodActivity extends AppCompatActivity {
     /**
      * Food
      */
-    private CardView rice;
+    private CardView rice, cirese, clatitecucafea, englishbreakfast, fruits1, cafea;
+    private CardView steak1, carbonara, salad, soup, steak2;
+    private CardView cartofipai, burger, hotdog, nuggets, gogosi, cola, onionrings, pizza;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,7 +39,29 @@ public class FoodActivity extends AppCompatActivity {
 
         init();
         displayCoins();
+
         buyFood(rice, "food1", 5, 5);
+        buyFood(cirese, "cirese", 1, 2);
+        buyFood(clatitecucafea, "clatitecucafea", 28, 16);
+        buyFood(englishbreakfast, "englishbreakfast", 28, 16);
+        buyFood(fruits1, "fruits1", 15, 13);
+        buyFood(cafea, "cafea", 4, 4);
+
+        buyFood(carbonara, "carbonara", 15, 25);
+        buyFood(salad, "salad", 8, 15);
+        buyFood(soup, "soup", 10, 12);
+        buyFood(steak1, "steak1", 30, 35);
+        buyFood(steak2, "steak2", 20, 22);
+
+        buyFood(cartofipai, "cartofipai", 7, 10);
+        buyFood(burger, "burger", 20, 27);
+        buyFood(cola, "cola", 4, 3);
+        buyFood(hotdog, "hotdog", 16, 20);
+        buyFood(nuggets, "nuggets", 10, 10);
+        buyFood(onionrings, "onionrings", 9, 8);
+        buyFood(gogosi, "gogosi", 12, 10);
+        buyFood(gogosi, "pizza", 10, 9);
+
         goBack();
     }
 
@@ -53,7 +77,30 @@ public class FoodActivity extends AppCompatActivity {
         foodRepository = new FoodRepository(this);
 
         // Food
+        // Breakfast
         rice = findViewById(R.id.rice);
+        cirese = findViewById(R.id.cirese);
+        clatitecucafea = findViewById(R.id.clatitecucafea);
+        englishbreakfast = findViewById(R.id.englishbreakfast);
+        fruits1 = findViewById(R.id.fruits1);
+        cafea = findViewById(R.id.cafea);
+
+        //Dinner
+        carbonara = findViewById(R.id.carbonara);
+        steak1 = findViewById(R.id.steak1);
+        salad = findViewById(R.id.salad);
+        soup = findViewById(R.id.soup);
+        steak2 = findViewById(R.id.steak2);
+
+        // Junk burger, hotdog, nuggets, gogosi, cola, onionrings, pizza
+        cartofipai = findViewById(R.id.cartofipai);
+        burger = findViewById(R.id.burger);
+        hotdog = findViewById(R.id.hotdog);
+        nuggets = findViewById(R.id.nuggets);
+        gogosi = findViewById(R.id.gogosi);
+        onionrings = findViewById(R.id.onionrings);
+        pizza = findViewById(R.id.pizza);
+        cola = findViewById(R.id.cola);
     }
 
     /**
